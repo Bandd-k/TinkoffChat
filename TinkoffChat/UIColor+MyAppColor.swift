9b355741959aeb94e74c0f2f826dcbda62050faa
Bandd-k <banddk1@gmail.com>
2017-03-20 16:48:41 +0300

    Added user first screen

---
+++ TinkoffChat/UIColor+MyAppColor.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+MyAppColor.swift
+//  TinkoffChat
+//
+//  Created by Denis Karpenko on 19.03.17.
+//  Copyright © 2017 Denis Karpenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    class func appColor() -> UIColor {
+        return UIColor(red: 255.0/255.0, green: 206.0/255.0, blue:
+            22.0/255.0, alpha: 1)
+    }
+}
--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -8,18 +8,160 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var colorText: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var aboutUserLabel: UILabel!
+    
+    @IBOutlet weak var aboutTextView: UITextView!
+    
+    @IBOutlet weak var userImageLabel: UILabel!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var colorButton1: UIButton!
+    
+    @IBOutlet weak var colorButton2: UIButton!
+    
+    @IBOutlet weak var colorButton3: UIButton!
+    
+    @IBOutlet weak var colorButton4: UIButton!
+    
+    @IBOutlet weak var colorButton5: UIButton!
+    
+    
+    
+    
+    let picker = UIImagePickerController()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.onTapAction))
+        view.addGestureRecognizer(tapGesture)
+        nameField.delegate = self
+        let tapImageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.imageTaped))
+        userImage.isUserInteractionEnabled = true
+        userImage.addGestureRecognizer(tapImageGestureRecognizer)
+        picker.delegate = self
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        // Debug info
+        print("######################################")
+        print("\(#function)")
+        self.printDebug()
+        print("######################################")
     }
+    override func viewWillAppear(_ animated: Bool) {
+        // Debug info
+        print("######################################")
+        print("\(#function)")
+        self.printDebug()
+        print("######################################")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Debug info
+        print("######################################")
+        print("\(#function)")
+        self.printDebug()
+        print("######################################")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        // Debug info
+        print("######################################")
+        print("\(#function)")
+        self.printDebug()
+        print("######################################")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        // Debug info
+        print("######################################")
+        print("\(#function)")
+        self.printDebug()
+        print("######################################")
+    }
+    
+    
+    
+    func printDebug(){
+        print(nameField.description)
+        print(colorText.description)
+        print(userImage.description)
+        print(aboutUserLabel.description)
+        print(aboutTextView.description)
+        print(userImageLabel.description)
+        print(saveButton.description)
+        print(nameField.description)
+        print(colorButton1.description)
+        print(colorButton2.description)
+        print(colorButton3.description)
+        print(colorButton4.description)
+        print(colorButton5.description)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func onTapAction(){
+        view.endEditing(true)
+    }
+    
+    func imageTaped(){
+        let alert = UIAlertController(title: "Выбрать фото профиля", message:nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Отмена", style: .cancel) { action in
+        })
+        alert.addAction(UIAlertAction(title: "Удалить фото", style: .destructive) { action in
+            self.userImage.image = UIImage(named: "placeholder-user")
+        })
+        alert.addAction(UIAlertAction(title: "Выбрать фото", style: .default) { action in
+            self.present(self.picker, animated: true, completion: nil)
+        })
+
+        self.present(alert, animated: true)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Debug info
+        print("######################################")
+        print("\(#function)")
+        self.printDebug()
+        print("######################################")
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func saveAction(_ sender: Any) {
+        print("Сохранение данных профиля")
+    }
+    
+    @IBAction func colorButtonTaped(_ sender: UIButton) {
+        colorText.textColor = sender.backgroundColor
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        self.userImage.image = chosenImage
+        // use the image
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
 
